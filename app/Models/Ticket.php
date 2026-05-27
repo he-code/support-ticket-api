d31893fc69b26e7e36d5ecbd18f34b96f485a3f3
he-code <henrysinche99@hotmail.com>
2026-05-27 15:11:28 -0500

    add ticket and user relationships

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+    'title',
+    'description',
+    'status',
+    'priority',
+    'user_id'
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
