58c214ab0f48734a5b57ea2bb182bb195fd1eeba
he-code <henrysinche99@hotmail.com>
2026-06-06 11:16:36 -0500

    Register ticket comments routes and relationship

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TicketController;
 use App\Http\Controllers\TicketCommentController;
+use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::apiResource('tickets', TicketController::class);
+
     Route::patch('/tickets/{ticket}/status', [TicketController::class, 'updateStatus']);
+
     Route::apiResource('tickets.comments', TicketCommentController::class)
-    ->only(['index', 'store', 'destroy']);
+        ->only(['index', 'store', 'destroy']);
 });
