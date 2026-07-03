7d0add5235188dbdf0b967af7aa25035d15abe8a
Henry <henrysinche99@hotmail.com>
2026-07-03 13:04:28 -0500

    Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,20 @@
 
 namespace App\Providers;
 
-use App\Models\Ticket;
-use App\Policies\TicketPolicy;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
-
-    protected $policies = [
-        Ticket::class => TicketPolicy::class,
-    ];
 }
